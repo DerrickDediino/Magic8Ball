8495097e3ff9a8b66447b0b7081eb09a5787b5b7
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-02-26 14:15:00 -0500

    Declared and initialized scanner

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,14 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //declare and initialize scanner//
+        Scanner keyboard = new Scanner(System.in);
+
+
     }
 }
