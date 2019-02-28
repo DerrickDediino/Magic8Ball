cffce3319c8296275e687f9a37e6287aa3a141e8
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-02-28 14:05:35 -0500

    Created all else if statements and responses.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,11 +6,61 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //declare and initialize scanner//
+        //if, else if, else
+
+        //declare and initialize scanner
         Scanner keyboard = new Scanner(System.in);
 
         //create integer
         Integer number = keyboard.nextInt();
 
+        //Creating possible responses
+
+        if(number == 1 || number == 100) {
+            System.out.println("Only if you think so.");
+        }
+
+        else if(number > 1 && number < 10) {
+            System.out.println("Maybe someday.");
+        }
+
+        else if(number >= 10 && number <20) {
+            System.out.println("Never.");
+        }
+
+        else if(number >= 20 && number <30) {
+            System.out.println("Always.");
+        }
+
+        else if(number >= 30 && number <40){
+            System.out.println("Of course.");
+        }
+
+        else if(number >= 40 && number <50) {
+            System.out.println("Of course not.");
+        }
+        else if(number >=50 && number <60) {
+            System.out.println("Why not?");
+        }
+
+        else if(number >=60 && number <70) {
+            System.out.println("Not if you keep that attitude.");
+        }
+
+        else if(number >=70 && number <80) {
+            System.out.println("Surely.");
+        }
+
+        else if(number >=80 && number <90) {
+            System.out.println("No way");
+        }
+
+        else if(number >=90 && number <100) {
+            System.out.println("Yeah, aight.");
+        }
+
+        else if(number < 0 || number > 100) {
+            System.out.println("Funny... that's not what I asked for.");
+        }
     }
 }
